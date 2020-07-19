b7ddde2ccb66ab91b2d485fe4fbf434f8867eae1
Keen Wilson <kwilson@finsignum.com>
2020-07-19 17:58:59 -0500

    Use iterator functions like enumerate, zip, iter, next

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-iterators-days-in-french
+
 Create an iterator over a collection of days in English and French
---
+++ main.py
@@ -0,0 +1,33 @@
+# use iterator functions like enumerate, zip, iter, next
+
+def main():
+  # define a list of days in English and French
+  days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+  daysFr = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"]
+
+  # TODO: use iter to create an iterator over a collection
+  i = iter(days)
+  print(next(i))
+  print(next(i))
+  print(next(i))
+
+  # TODO: iterate using a function and a sentinel
+  with open("testfile.txt", "r") as fp:
+    for line in iter(fp.readline, ''):
+      # When the value returns equal to an empty string (a sentinel), the iterator will stop
+      print(line)
+
+  # TODO: use regular iteration over the days
+  for m in range(len(days)):
+    print(m+1, days[m])
+
+  # TODO: using enumerate reduces code and provides a counter
+  for i, m in enumerate(days, start=1):
+    print(i, m)
+
+  # TODO: use zip to combine sequences
+  for i, m in enumerate(zip(days, daysFr), start=1):
+    print(i, m[0], "=", m[1], "in French")
+
+if __name__ == "__main__":
+  main()
